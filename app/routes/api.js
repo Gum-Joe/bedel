a3f17f24deee80bc17191c106c914503643599fd
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-06-25 21:10:47 +0100

    :lipstick: Added a not found screen
    :lipstick: Added a not found screen
    :art: Added json loading in webpack

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,10 +3,18 @@
  * Module dependencies
  */
 const express = require('express');
+const pack = require('../../package');
 const path = require('path');
 
 // Init
 const app = express();
+
+// GET /info/version
+app.get('/info/version', function(req, res) {
+  res.json({
+    version: pack.version
+  });
+});
 
 // GET /session/user
 app.get('/session/user', (req, res) => {
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   module: {
       loaders: [
+        { test: /\.json$/, exclude: /node_modules/, loaders: [ "json-loader" ] },
         { test: /\.jsx$/, exclude: /node_modules/, loaders: [ "react-hot", "babel-loader"] },
         { test: /\.js$/, exclude: /node_modules/, loaders: [ "react-hot", "babel-loader"] },
         { test: /\.scss$/, exclude: /node_modules/, loaders: ["style", "css", "sass"] },
