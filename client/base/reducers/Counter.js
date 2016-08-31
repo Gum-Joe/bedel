8f3696d1d032cd39cc671afbca7b5701901debe9
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-08-31 17:35:21 +0100

    :shirt: Code Climate improvments
    :shirt: Code Climate improvments in client/base/reducers/Counter.js
    (#34)

--- client/base/reducers/Counter.js
+++ client/base/reducers/Counter.js
@@ -9,18 +9,29 @@ const intial = {
 
 // Reducer
 export const counter = (state = intial, action) => {
+  /**
+   * Do a sum
+   *
+   * @param oldState {Object} current state
+   * @param prop {String} prop of oldState to change
+   * @param int {Number} number to add (positive or negative)
+   * @return {Object} new state
+   */
+  const doSum = (oldState, prop, int) => {
+    const newState = Object.assign({}, oldState);
+    newState[action.prop] += int;
+    return newState;
+  };
+
+  // Reducer logic
   switch (action.type) {
     case PLUS: {
-      const newState = Object.assign({}, state);
-      newState[action.prop] += action.increment || 1;
-      return newState;
+      return doSum(state, action.prop, action.increment || 1);
     }
     case MINUS: {
-      const newState = Object.assign({}, state);
-      newState[action.prop] -= action.increment || 1;
-      return newState;
+      return doSum(state, action.prop, -action.increment || -1);
     }
     default:
-    return state;
+      return state;
   }
 };
