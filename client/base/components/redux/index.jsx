cc0349fd525c0f9f7db2108e6a53acbe7f758d83
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-11-27 13:38:25 +0000

    :lipstick: Start to redo UI
    :lipstick: Start to redo UI
    :lipstick: Removed current UI components
    :lipstick: Installed Semantic UI
    :rocket: Build semantic from root gulpfile
    :art: Added nodejs-dashboard
    :shirt: Disabled eslint rule global-require

--- client/base/components/redux/index.jsx
+++
@@ -1,4 +0,0 @@
-// Index of redux components
-import UsernameComp from './username';
-
-export const Username = UsernameComp;
--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
 // Entry file for react
 // Polyfill
 import 'babel-polyfill';
+
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -9,16 +10,18 @@ import 'hamburgers/dist/hamburgers.min.css';
 // Bootstrap plugins
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// Router
+import { Router, Route, IndexRoute } from 'react-router';
+
 // The actual code
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { routes } from './routes';
 import configureStore from './base/store/configureStore';
 
 const store = configureStore();
 render((
   <Provider store={store}>
-    {routes}
+
   </Provider>
 ), document.getElementById('main'));
