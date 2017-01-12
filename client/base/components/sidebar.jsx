15c4ad3ad3945848878d29b9acc3d7b09cae212f
Gum-Joe <kishansambhi@hotmail.co.uk>
2017-01-12 20:27:49 +0000

    :lipstick: Added sidebar animation
    :lipstick: Added sidebar animation (sidebatr now opens and closes) +
    sidebar item label & animations (fade in and out)
    :checkered_flag: Need to check compatibilty with edge

--- client/base/components/sidebar.jsx
+++ client/base/components/sidebar.jsx
@@ -11,7 +11,10 @@ class Item extends Component {
   render() {
     return (
       <div className={classnames("bedel-item", { active: this.props.active })}>
-        <FontAwesome name={this.props.icon} />
+        <div className="bedel-inner-item">
+          <FontAwesome name={this.props.icon} />
+        </div>
+        <p>{this.props.text}</p>
       </div>
     )
   }
@@ -19,7 +22,8 @@ class Item extends Component {
 
 Item.propTypes = {
   active: PropTypes.bool,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 }
 
 export class Sidebar extends Component {
@@ -29,22 +33,45 @@ export class Sidebar extends Component {
       open: false
     }
   }
+  animateSidebar() {
+    if (!this.state.open) {
+      $('.bedel-sidebar').addClass('sidebar-animate');
+      $('.bedel-sidebar').removeClass('sidebar-animate-close');
+      $('.body').addClass('body-animate');
+      $('.body').removeClass('body-animate-close');
+      $('.bedel-navbar').addClass('navbar-animate');
+      $('.bedel-navbar').removeClass('navbar-animate-close');
+    } else {
+      $('.bedel-sidebar').addClass('sidebar-animate-close');
+      $('.bedel-sidebar').removeClass('sidebar-animate');
+      $('.body').addClass('body-animate-close');
+      $('.body').removeClass('body-animate');
+      $('.bedel-navbar').addClass('navbar-animate-close');
+      $('.bedel-navbar').removeClass('navbar-animate');
+    }
+    this.setState({
+      open: !this.state.open
+    })
+  }
   render() {
     return (
       <div className="bedel-sidebar">
-        <div className="bedel-item" id="hamburger">
-          <button>
-            <div></div>
-            <div></div>
-            <div></div>
-          </button>
+        <div className="bedel-item" id="hamburger-wrapper">
+          <div className="bedel-inner-item" id="hamburger">
+            <button onClick={this.animateSidebar.bind(this)}>
+              <div></div>
+              <div></div>
+              <div id="last-hamburger"></div>
+            </button>
+          </div>
         </div>
-        <Item icon="dashboard" />
-        <Item icon="comments" />
-        <Item icon="line-chart" />
-        <Item icon="question" />
-        <Item icon="comment" />
-        <Item icon="cog" />
+        <Item icon="dashboard" text="Dashboard" />
+        <Item icon="cube" text="Apps" />
+        <Item icon="comments" text="Messages" />
+        <Item icon="line-chart" text="Stats" />
+        <Item icon="question" text="Help" />
+        <Item icon="comment" text="Feedback" />
+        <Item icon="cog" text="Settings" />
       </div>
     );
   }
